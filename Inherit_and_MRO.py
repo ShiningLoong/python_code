da31f1673e20aee84644c9f6753599a1cf619022
LF-work <123@123.com>
2019-04-17 15:41:52 +0800

    modified

--- Inherit_and_MRO.py
+++ Inherit_and_MRO.py
@@ -1,13 +1,57 @@
 class A:
     def __init__(self):
-        print(A)
+        print('A')
 
 
 class B(A):
     def __init__(self):
-        A.__init__(self)   # this self cannot be omitted
+        A.__init__(self)
+        print('B')# this self cannot be omitted
         pass
 
 
-b = B()
+class B2(A):
+    def __init__(self):
+        A.__init__(self)
+        print('B2')# this self cannot be omitted
+        pass
+
+
+class C(B, B2):
+    def __init__(self):
+        B.__init__(self)
+        B2.__init__(self)
+        print('C')
+
+
+c = C()
+
+print("_______________________________________________")
+
+
+class B1_super(A):
+    def __init__(self):
+        super().__init__()
+        print("B1")
+
+
+class B2_super(A):
+    def __init__(self):
+        super().__init__()
+        print("B2")
+
+
+class C_super(B1_super, B2_super):
+    def __init__(self):
+        super().__init__()  # super does not need to pass "self"
+        print("C")
+
+
+c_super = C_super()
+print(C_super.__mro__)
+
+# calling sequence of init method is the reverse of the MRO list
+# with super() we don't need to bother with every init method of base classes, just one super() is enough
+# with super() the we also avoid to repeatedly call the same base class's init method
+print("_______________________________________________")
 
